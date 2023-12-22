77dd1e1281681191ce68e6c408338f5c310ade03
PaulPruenster <paul.pruenster@gmail.com>
2023-12-22 14:23:41 +0100

    place jump pad into scene and minor naming convetion fix

--- jumping_pad.gd
+++ jumping_pad.gd
@@ -2,15 +2,6 @@ extends Node2D
 
 @export var jumping_force_y = -1050
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func apply_effect(player: CharacterBody2D):
 	player.velocity.y = jumping_force_y
 
